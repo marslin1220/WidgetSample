64da2efb77d22097988fda32e1395345a6caf6f7
marslin <marslin@gmail.com>
2014-10-28 16:42:42 +1000

    Add 'HelloToday' widget target into project.

---
+++ HelloToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  HelloToday
+//
+//  Created by 林 政龍 on 2014/10/28.
+//  Copyright (c) 2014年 marstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
